27f2632cfea423b93917e34c78ae32427cfd465e
Austin Gomez <austingomez@hotmail.ca>
2019-05-06 12:17:19 -0400

    Added prettier auto-formatting on commit

--- src/lib/components/Grid.js
+++ src/lib/components/Grid.js
@@ -1,142 +1,164 @@
-import React, { useState, useEffect, useCallback } from 'react'
-import PropTypes from 'prop-types'
-import useWindowSize from '../hooks/UseWindowSize'
-import Lightbox from './Lightbox'
-import './Grid.css'
+import React, { useState, useEffect, useCallback } from "react";
+import PropTypes from "prop-types";
+import useWindowSize from "../hooks/UseWindowSize";
+import Lightbox from "./Lightbox";
+import "./Grid.css";
 
 const Grid = React.forwardRef(({ images, rowHeight, margin = 0 }, ref) => {
-    let windowSize = useWindowSize()
-    const [minAspectRatio, setMinAspectRatio] = useState()
-    const [rows, setRows] = useState([])
-    const [showLightbox, setShowLightbox] = useState(false)
-    const [selectedIndex, setSelectedIndex] = useState()
+  let windowSize = useWindowSize();
+  const [minAspectRatio, setMinAspectRatio] = useState();
+  const [rows, setRows] = useState([]);
+  const [showLightbox, setShowLightbox] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState();
 
-    // We'll use this value to calculate how many pictures we need in a row.
-    // The "min" aspect ratio is the aspect ratio that will allow the row to
-    // span the correct length while being between minRowHeight and maxRowHeight
-    const calcMinAspectRatio = useCallback(() => {
-        return (ref.current.parentNode.clientWidth) / rowHeight
-    }, [rowHeight, ref])
+  // We'll use this value to calculate how many pictures we need in a row.
+  // The "min" aspect ratio is the aspect ratio that will allow the row to
+  // span the correct length while being between minRowHeight and maxRowHeight
+  const calcMinAspectRatio = useCallback(() => {
+    return ref.current.parentNode.clientWidth / rowHeight;
+  }, [rowHeight, ref]);
 
-    const handleImageClick = (e) => {
-        setSelectedIndex(e.target.getAttribute('data-index'))
-        setShowLightbox(true)
+  const handleImageClick = e => {
+    setSelectedIndex(e.target.getAttribute("data-index"));
+    setShowLightbox(true);
+  };
+
+  // Creates <img/> tags from image and row metadata.
+  //
+  const imageDataToImgTag = useCallback(
+    (image, totalIndex, rowIndex, row, width) => {
+      const calculatedRowHeight = ref.current.parentNode.clientWidth / width;
+
+      // Calculate the dimensions and margin of each image. This needs
+      // to be inline since we need some values from the JS.
+      let imageStyle = {
+        height: calculatedRowHeight + "px",
+
+        // Take back out the margin from the ratio.
+        width: calculatedRowHeight * image[1] - margin + "px",
+
+        // Don't add margin to the last item in a row
+        marginRight: rowIndex === row.length - 1 ? 0 : margin + "px"
+      };
+      return (
+        <img
+          className="grid-img"
+          style={imageStyle}
+          data-index={totalIndex}
+          onClick={handleImageClick}
+          src={image[0].src}
+          alt={image[0].alt}
+          key={"img_" + image[0].id + "_" + image[1]}
+        />
+      );
+    },
+    [ref, margin]
+  );
+
+  // Build the rows of the grid. Each row must have an aspect ratio of at least minAspectRatio.
+  // Then, each image in the row is scaled up to fill the desired width of the row, while maintaining
+  // the aspect ratio of each photo in the row.
+  const makeRows = useCallback(() => {
+    let allRows = [];
+    let row = [];
+    let width = 0;
+    for (let i = 0; i < images.length; i++) {
+      let image = images[i];
+
+      // Add the margin into the ratio.
+      let ratio = image.width / image.height;
+
+      // If we're less than the min aspectRatio then keep adding more items to the row.
+      if (width <= minAspectRatio && i !== images.length - 1) {
+        // Add the ratio contributed by the margin.
+        ratio += margin / image.height;
+        row.push([image, ratio]);
+        width += ratio;
+      } else {
+        if (i === images.length - 1) {
+          row.push([image, ratio]);
+          width += ratio;
+        }
+        let imageElements = [];
+        for (let j = 0; j < row.length; j++) {
+          imageElements.push(
+            imageDataToImgTag(row[j], i - row.length + j, j, row, width)
+          );
+        }
+        // Add the finished row to the list of all rows.
+        allRows.push(imageElements);
+
+        // Start a new row with the current image as the first image
+        row = [[image, ratio]];
+        width = ratio;
+      }
     }
 
-    // Creates <img/> tags from image and row metadata.
-    // 
-    const imageDataToImgTag = useCallback((image, totalIndex, rowIndex, row, width) => {
-        const calculatedRowHeight = ref.current.parentNode.clientWidth / width
+    // This style also needs to be inline since we use the padding prop.
+    // (Would be nice to use something like styled-components for this instead)
+    const divStyle = {
+      display: "flex",
+      marginBottom: margin + "px"
+    };
+    setRows(
+      allRows.map((row, index) => (
+        <div className="grid-row" style={divStyle} key={"row_" + index}>
+          {row}
+        </div>
+      ))
+    );
+  }, [images, margin, minAspectRatio, imageDataToImgTag]);
 
-        // Calculate the dimensions and margin of each image. This needs
-        // to be inline since we need some values from the JS.
-        let imageStyle = {
-            height: calculatedRowHeight + "px",
+  // Recalculate min aspect ratio when the window size changes.
+  useEffect(() => {
+    setMinAspectRatio(calcMinAspectRatio());
+  }, [calcMinAspectRatio, windowSize]);
 
-            // Take back out the margin from the ratio.
-            width: calculatedRowHeight * image[1] - margin + "px",
+  // This isn't optimal. Ideally we don't want to call makeRows every time the aspect ratio changes.
+  // There should be a resizeRows method that just goes through each row and recalculates and resizes
+  // each row/image.
+  useEffect(() => {
+    if (minAspectRatio) {
+      makeRows();
+    }
+  }, [minAspectRatio, makeRows]);
 
-            // Don't add margin to the last item in a row
-            marginRight: rowIndex === row.length - 1 ? 0 : margin + "px"
-        }
-        return <img
-            className="grid-img"
-            style={imageStyle}
-            data-index={totalIndex}
-            onClick={handleImageClick}
-            src={image[0].src}
-            alt={image[0].alt}
-            key={"img_" + image[0].id + "_" + image[1]} />
-    }, [ref, margin])
-
-
-    // Build the rows of the grid. Each row must have an aspect ratio of at least minAspectRatio.
-    // Then, each image in the row is scaled up to fill the desired width of the row, while maintaining
-    // the aspect ratio of each photo in the row.
-    const makeRows = useCallback(() => {
-        let allRows = []
-        let row = []
-        let width = 0
-        for (let i = 0; i < images.length; i++) {
-            let image = images[i]
-
-            // Add the margin into the ratio.
-            let ratio = image.width / image.height
-
-            // If we're less than the min aspectRatio then keep adding more items to the row.
-            if (width <= minAspectRatio && i !== images.length - 1) {
-                // Add the ratio contributed by the margin.
-                ratio += margin / image.height
-                row.push([image, ratio])
-                width += ratio
-            }
-            else {
-                if (i === images.length - 1) {
-                    row.push([image, ratio])
-                    width += ratio
-                }
-                let imageElements = []
-                for (let j = 0; j < row.length; j++) {
-                    imageElements.push(imageDataToImgTag(row[j], i - row.length + j, j, row, width))
-                }
-                // Add the finished row to the list of all rows.
-                allRows.push(imageElements)
-
-                // Start a new row with the current image as the first image
-                row = [[image, ratio]]
-                width = ratio
-            }
-        }
-
-        // This style also needs to be inline since we use the padding prop.
-        // (Would be nice to use something like styled-components for this instead)
-        const divStyle = {
-            display: "flex",
-            marginBottom: margin + "px"
-        }
-        setRows(allRows.map((row, index) => <div className="grid-row" style={divStyle} key={"row_" + index}>{row}</div>))
-    }, [images, margin, minAspectRatio, imageDataToImgTag])
-
-    // Recalculate min aspect ratio when the window size changes.
-    useEffect(() => {
-        setMinAspectRatio(calcMinAspectRatio())
-    }, [calcMinAspectRatio, windowSize])
-
-    // This isn't optimal. Ideally we don't want to call makeRows every time the aspect ratio changes.
-    // There should be a resizeRows method that just goes through each row and recalculates and resizes
-    // each row/image.
-    useEffect(() => {
-        if (minAspectRatio) {
-            makeRows()
-        }
-    }, [minAspectRatio, makeRows])
-
-    return (
-        <>
-            <div className="grid-container" ref={ref}>
-                {rows}
-            </div>
-            {showLightbox ?
-                <Lightbox
-                    selectedImage={images[selectedIndex]}
-                    onClose={() => setShowLightbox(false)}
-                    onPrev={() => setSelectedIndex(selectedIndex => (+selectedIndex - 1 + images.length) % images.length)}
-                    onNext={() => setSelectedIndex(selectedIndex => (+selectedIndex + 1) % images.length)} />
-                : null
-            }
-        </>
-    )
-})
+  return (
+    <>
+      <div className="grid-container" ref={ref}>
+        {rows}
+      </div>
+      {showLightbox ? (
+        <Lightbox
+          selectedImage={images[selectedIndex]}
+          onClose={() => setShowLightbox(false)}
+          onPrev={() =>
+            setSelectedIndex(
+              selectedIndex =>
+                (+selectedIndex - 1 + images.length) % images.length
+            )
+          }
+          onNext={() =>
+            setSelectedIndex(
+              selectedIndex => (+selectedIndex + 1) % images.length
+            )
+          }
+        />
+      ) : null}
+    </>
+  );
+});
 
 Grid.propTypes = {
-    images: PropTypes.arrayOf(
-        PropTypes.shape({
-            src: PropTypes.string.isRequired,
-            height: PropTypes.number.isRequired,
-            width: PropTypes.number.isRequired
-        })).isRequired,
-    rowHeight: PropTypes.number.isRequired,
-    margin: PropTypes.number
-}
+  images: PropTypes.arrayOf(
+    PropTypes.shape({
+      src: PropTypes.string.isRequired,
+      height: PropTypes.number.isRequired,
+      width: PropTypes.number.isRequired
+    })
+  ).isRequired,
+  rowHeight: PropTypes.number.isRequired,
+  margin: PropTypes.number
+};
 
-export default Grid
+export default Grid;
